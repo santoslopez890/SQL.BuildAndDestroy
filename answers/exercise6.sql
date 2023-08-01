f991d8324bdcdeb1530631549b79fbc233f6d3bd
santos <santoslopez890@gmail.com>
2023-08-01 13:58:20 -0400

    finished 1-9

---
+++ answers/exercise6.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Users (
+ PersonID int,
+  LastName varchar(255),
+  FirstName varchar(255),
+   Address varchar(255),
+   City varchar(255)
+   Birthday Date;
+
+);
---
+++ answers/exercise8.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Students (
+  StudentName varchar(255),
+   Address varchar(255),
+   City varchar(255)
+   PostalCode int,
+   Country varchar(255);
+);
